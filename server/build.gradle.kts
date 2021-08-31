5239ce0a3d1b6168ba99fff0927a652f670de3e7
alexandre.okita <alexandre.okita@MacBook-Pro-de-itech.local>
2021-08-31 11:06:01 -0300

    first commit

---
+++ server/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.5.20"
+}
+
+group = "com.jetbrains.handson"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val ktor_version: String by project
+val logback_version: String by project
+
+tasks.create("stage") {
+    dependsOn("installDist")
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-websockets:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+}
